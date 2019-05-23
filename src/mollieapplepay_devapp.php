2327f5dc49512a4f4f33ba2080996abf35072d67
Philipp <philipp.esswein@me.com>
2019-05-23 17:03:42 +0200

    Create mollieapplepay_devapp.php

---
+++ src/mollieapplepay_devapp.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/mollie/mollie.php';
+
+function mollieapplepay_devapp_config()
+{
+    $config = mollie_config();
+    
+    $config = array_merge($config, array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Mollie ApplePay'
+        )
+    ));
+    
+    return $config;
+    
+}
+
+function mollieapplepay_devapp_link($params)
+{
+    return mollie_link($params, \Mollie\Api\Types\PaymentMethod::APPLEPAY);
+}
